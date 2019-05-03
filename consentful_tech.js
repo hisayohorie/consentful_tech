c98b69113ab5f934531f9519547980940f53bfee
hisayohorie <hisayohorie@gmail.com>
2019-05-03 16:38:18 -0400

    Adding touchstart events on humburger menu

--- consentful_tech.js
+++ consentful_tech.js
@@ -2,16 +2,14 @@ document.addEventListener('DOMContentLoaded',function(){
 	const humburger = document.getElementById('humburger');
 	const nav = document.querySelector('.nav');
 	const close = document.getElementById('humburger_close');
-	humburger.addEventListener('click', function(){
+
+	humburger.addEventListener('touchstart', function(){
 		nav.classList.toggle('modal_close');
 		nav.classList.toggle('modal_open');
 		humburger.style.display="none";
 		close.style.display="block";
-		
-		
-
 	});
-	close.addEventListener('click', function(){
+	close.addEventListener('touchstart', function(){
 		nav.classList.toggle('modal_close');
 		nav.classList.toggle('modal_open');
 		close.style.display= "none"
