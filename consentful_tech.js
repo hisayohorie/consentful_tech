e00a555671397cac2e4ad34e54e5262da4773e85
hisayohorie <hisayohorie@gmail.com>
2019-05-04 14:48:39 -0400

    Fixing the overflowing right by taking out the width of header

--- consentful_tech.js
+++ consentful_tech.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded',function(){
 	const nav = document.querySelector('.nav');
 	const close = document.getElementById('humburger_close');
 
-	humburger.addEventListener('touchstart', function(){
-		nav.classList.toggle('modal_close');
-		nav.classList.toggle('modal_open');
-		humburger.style.display="none";
-		close.style.display="block";
-	});
-	close.addEventListener('touchstart', function(){
-		nav.classList.toggle('modal_close');
-		nav.classList.toggle('modal_open');
-		close.style.display= "none"
-		humburger.style.display = "block"
-		humburger.style.display = "grid"	
-		
-	})
+	const humburgerEvents = ['click', 'touchstart'];
+	for (let i = humburgerEvents.length - 1; i >= 0; i--) {
+		const humburgerEvent = humburgerEvents[i];
+		humburger.addEventListener(humburgerEvent, function(){
+			nav.classList.toggle('modal_close');
+			nav.classList.toggle('modal_open');
+			humburger.style.display="none";
+			close.style.display="block";
+		});
+		close.addEventListener(humburgerEvent, function(){
+			nav.classList.toggle('modal_close');
+			nav.classList.toggle('modal_open');
+			close.style.display= "none";
+			humburger.style.display = "block";
+			humburger.style.display = "grid";
+		});
+	}
+
 });
