2f1015de35729ace927f29c6dc13f01ad75d225a
hisayohorie <hisayohorie@gmail.com>
2019-05-08 21:21:29 -0400

    Adding the border to the title

--- consentful_tech.js
+++ consentful_tech.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',function(){
 	const humburger = document.getElementById('humburger');
 	const nav = document.querySelector('.nav');
 	const close = document.getElementById('humburger_close');
+	const h1 = document.querySelector('h1');
 
 	const humburgerEvents = ['click', 'touchstart'];
 	for (let i = humburgerEvents.length - 1; i >= 0; i--) {
@@ -9,12 +10,14 @@ document.addEventListener('DOMContentLoaded',function(){
 		humburger.addEventListener(humburgerEvent, function(){
 			nav.classList.toggle('modal_close');
 			nav.classList.toggle('modal_open');
+			h1.classList.toggle('title_underline');
 			humburger.style.display="none";
 			close.style.display="block";
 		});
 		close.addEventListener(humburgerEvent, function(){
 			nav.classList.toggle('modal_close');
 			nav.classList.toggle('modal_open');
+			h1.classList.toggle('title_underline');
 			close.style.display= "none";
 			humburger.style.display = "block";
 			humburger.style.display = "grid";
