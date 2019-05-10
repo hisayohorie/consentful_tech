525768d1856928f96d3d5953e1a92b5524121627
hisayohorie <hisayohorie@gmail.com>
2019-05-10 10:47:04 -0400

    Reorganizing the js for hamburger, fixing the horizontal scroll on mobile

--- consentful_tech.js
+++ consentful_tech.js
@@ -5,22 +5,39 @@ document.addEventListener('DOMContentLoaded',function(){
 	const h1 = document.querySelector('h1');
 
 	const humburgerEvents = ['click', 'touchstart'];
+
 	for (let i = humburgerEvents.length - 1; i >= 0; i--) {
 		const humburgerEvent = humburgerEvents[i];
-		humburger.addEventListener(humburgerEvent, function(){
+		humburger.addEventListener(humburgerEvent, function(event){
+			if(event.handled === false) {
+				return
+			} 
+	        event.stopPropagation();
+	        event.preventDefault();
+	        event.handled = true;
+			console.log('hi')
+
 			nav.classList.toggle('modal_close');
 			nav.classList.toggle('modal_open');
 			h1.classList.toggle('title_underline');
 			humburger.style.display="none";
 			close.style.display="block";
 		});
-		close.addEventListener(humburgerEvent, function(){
+		close.addEventListener(humburgerEvent, function(event){
+			if(event.handled === false) {
+				return
+			}
+	        event.stopPropagation();
+	        event.preventDefault();
+	        event.handled = true;
 			nav.classList.toggle('modal_close');
 			nav.classList.toggle('modal_open');
 			h1.classList.toggle('title_underline');
 			close.style.display= "none";
 			humburger.style.display = "block";
 			humburger.style.display = "grid";
+			console.log('hello')
+
 		});
 	}
 
