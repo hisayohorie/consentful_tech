0f222eee18a56851094a855ea0919653d123643e
hisayohorie <hisayohorie@gmail.com>
2019-05-11 13:47:20 -0400

    Adding form modal and js function

--- consentful_tech.js
+++ consentful_tech.js
@@ -1,44 +1,50 @@
 document.addEventListener('DOMContentLoaded',function(){
 	const humburger = document.getElementById('humburger');
-	const nav = document.querySelector('.nav');
-	const close = document.getElementById('humburger_close');
-	const h1 = document.querySelector('h1');
+	const nav = document.querySelector('nav');
+	const navClose = document.getElementById('humburger_close');
 
-	const humburgerEvents = ['click', 'touchstart'];
+	const modalEvents = ['click', 'touchstart'];
 
-	for (let i = humburgerEvents.length - 1; i >= 0; i--) {
-		const humburgerEvent = humburgerEvents[i];
-		humburger.addEventListener(humburgerEvent, function(event){
-			if(event.handled === false) {
-				return
-			} 
-	        event.stopPropagation();
-	        event.preventDefault();
-	        event.handled = true;
-			console.log('hi')
+	const about_this_form = document.getElementById('about_this_form');
+	const form_modal = document.getElementById('form_modal');
+	const form_close = document.querySelector('.form_modal_close');
 
-			nav.classList.toggle('modal_close');
-			nav.classList.toggle('modal_open');
-			h1.classList.toggle('title_underline');
-			humburger.style.display="none";
-			close.style.display="block";
+
+	for (let i = modalEvents.length - 1; i >= 0; i--) {
+		const modalEvent = modalEvents[i];
+
+		humburger.addEventListener(modalEvent, function(event){
+ 			toggleModal(event, nav, navClose, humburger);
 		});
-		close.addEventListener(humburgerEvent, function(event){
-			if(event.handled === false) {
-				return
-			}
-	        event.stopPropagation();
-	        event.preventDefault();
-	        event.handled = true;
-			nav.classList.toggle('modal_close');
-			nav.classList.toggle('modal_open');
-			h1.classList.toggle('title_underline');
-			close.style.display= "none";
-			humburger.style.display = "block";
-			humburger.style.display = "grid";
-			console.log('hello')
 
+		navClose.addEventListener(modalEvent, function(event){
+			toggleModal(event, nav, humburger, navClose);
 		});
+
+
+		about_this_form.addEventListener(modalEvent, function(event){
+			// open form modal
+			form_modal.style.display="block";
+		})
+
+		form_close.addEventListener(modalEvent, function(event){
+			// close form modal
+			form_modal.style.display="none";
+		})
+
+		}
+
+	function toggleModal(event, modalContainer, iconToShow, iconToHide){
+		if(event.handled === false) {
+			return
+		}
+	    event.stopPropagation();
+	    event.preventDefault();
+	    event.handled = true;
+
+		modalContainer.classList.toggle('modal_close');
+		modalContainer.classList.toggle('modal_open');
+		iconToHide.style.display="none";
+		iconToShow.style.display="block";
 	}
-
 });
