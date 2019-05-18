19a22cbcb551de5ad1973f88b42291309bf096de
hisayohorie <hisayohorie@gmail.com>
2019-05-17 22:45:15 -0400

    Making all the changes

--- consentful_tech.js
+++ consentful_tech.js
@@ -1,17 +1,39 @@
 document.addEventListener('DOMContentLoaded',function(){
-	const humburger = document.getElementById('humburger');
-	const nav = document.querySelector('nav');
-	const navClose = document.getElementById('humburger_close');
 
-	const modalEvents = ['click', 'touchstart'];
+	var humburger = document.getElementById('humburger');
+	var nav = document.querySelector('nav');
+	var navClose = document.getElementById('humburger_close');
 
-	const about_this_form = document.getElementById('about_this_form');
-	const form_modal = document.getElementById('form_modal');
-	const form_close = document.querySelector('.form_modal_close');
+	var modalEvents = ['click', 'touchstart'];
 
+	var about_this_form = document.getElementById('about_this_form');
+	var form_modal = document.getElementById('form_modal');
+	var form_close = document.querySelector('.form_modal_close');
 
-	for (let i = modalEvents.length - 1; i >= 0; i--) {
-		const modalEvent = modalEvents[i];
+  	$('nav a[href*="#"]:not([href="#"])').click(function(e) {
+  		e.preventDefault();
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				target.focus(); // Setting focus
+				if (target.is(":focus")){ // Checking if the target was focused
+					return false;
+				} else {
+					target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+					target.focus(); // Setting focus
+				};
+				var baseLoc = location.pathname.split('#')[0];
+				location = baseLoc + '#' + target.attr('id');
+			}
+		}
+ 	});
+
+	for (var i = modalEvents.length - 1; i >= 0; i--) {
+		var modalEvent = modalEvents[i];
 
 		humburger.addEventListener(modalEvent, function(event){
  			toggleModal(event, nav, navClose, humburger);
